8ff0c096596471e9c4abdcbf169fc9381cc6a7f7
Christian Kohler <christian.kohler@zuehlke.com>
2015-02-10 16:36:12 +0100

    no modularization

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-define('app', ['car'], function(car) {
-    car.start();
+var statusLightsOn = false;
+var statusMotorRunning = false;
+var statusCarReady = statusLightsOn && statusMotorRunning;
+
+function start() {
+    logToView('starting...');
+
+    runMotor();
+    switchOnLights();
+}
+
+function runMotor() {
+    statusMotorRunning = true;
+    logToView('motor is running... "puff puff"');
+}
+
+function switchOnLights() {
+    statusLightsOn = true;
+    logToView('Headlights now on...');
+}
+
+function logToView(message) {
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(message));
+    document.querySelector('#output').appendChild(li);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    start();
+    console.log("Status:", statusCarReady ? "car running" : "car not running");
 });
-
-
-requirejs.config({
-    baseUrl: './'
-});
-
-
-requirejs(['app']);
